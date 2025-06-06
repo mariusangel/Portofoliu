f277ad0c1ff21a72174c9127eb38e237c65a9d06
mariusangel <147443299+mariusangel@users.noreply.github.com>
2025-06-06 17:33:54 +0300

    Add files via upload

---
+++ Website with login and register/script.js
@@ -0,0 +1,26 @@
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', (event)=> {
+  event.preventDefault();
+  wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', (event)=> {
+  event.preventDefault();
+  wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', (event)=> {
+  event.preventDefault();
+  wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+  wrapper.classList.remove('active-popup');
+});
+
